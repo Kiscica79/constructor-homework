4417b68458b38dd18ec637ab6779323b11c84810
Kiscica79 <oroszildiko79@gmail.hu>
2023-06-09 17:16:31 +0200

    Hello, ez a házi feladatom

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,46 @@
 public class BankAccount {
+
     // TODO
+    // 1.a
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    // 2.b paraméteres konstruktőr
+    public BankAccount(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public BankAccount(String accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+    // default konstruktőr
+    public BankAccount() {
+        this("44444444", "Mekk Elek", 33333.333);
+    }
+
+    // 1.b setterek
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    // 1.b getterek
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+    public double getBalance() {
+        return balance;
+    }
+
+
+
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+
+
         Car myCar = new Car("Toyota", "Camry", 2022);
         System.out.println(myCar.getBrand()); // Output: Toyota
 
         Car defaultCar = new Car();
         System.out.println(defaultCar.getModel()); // Output: Unknown
+
+        // 1.c példányosítás, objektumok
+        // 2.b paraméteres konstruktor
+        BankAccount customerAccount = new BankAccount("77777777-77777777-77777777");
+        customerAccount.setAccountNumber("11111111-11111111-11111111");
+        customerAccount.setAccountHolder("Nagy Sándor");
+        customerAccount.setBalance(57893333);
+        System.out.println(customerAccount.getAccountNumber()+" a bankszámla száma.");
+        System.out.println(customerAccount.getAccountHolder()+" a számlatulajdonos neve");
+        System.out.println(customerAccount.getBalance()+" Ft van a számláján.");
+
+        BankAccount myAccount = new BankAccount("0000000000000000", "Gipsz Jakab", 444444444);
+        System.out.println(myAccount.getAccountHolder()+" a nevem "+
+                myAccount.getAccountNumber()+" ez a számlaszámom "+
+                myAccount.getBalance()+" és ez az egyenlegem ");
+
+
+
     }
 }
